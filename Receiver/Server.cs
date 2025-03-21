d79fcc06ffee62127e9d99ccf6e999ffa6d9fc95
Arunkumar Anil Jolly <arunkumar.aniljolly@hrblock.com>
2025-03-22 01:15:25 +0530

    +

--- Receiver/Server.cs
+++ Receiver/Server.cs
@@ -17,22 +17,62 @@ internal static class Server
         while (true)
         {
             Console.Write("Waiting for a connection... ");
-            TcpClient client = server.AcceptTcpClient();
-            _ = Task.Run(() => ProcessClient(client));
+            TcpClient client = await server.AcceptTcpClientAsync();
+            _ = Task.Run(() => ProcessClient4(client));
         }
+    }
+
+    internal static async Task ProcessClient4(TcpClient client)
+    {
+        // v3 
+        NetworkStream stream = client.GetStream();
+
+        FileStream inputFile = new($"{Constants.ServerOutputFolder}/IMG-20250318-WA0001.jpg", FileMode.Create);
+        StreamReader streamReader = new(stream, Encoding.UTF8);
+        string emo = streamReader.ReadLine();
+        Console.WriteLine(emo);
+        await stream.CopyToAsync(inputFile);
+        client.Close();
+    }
+    internal static async Task ProcessClient3(TcpClient client)
+    {
+        // v2.2 hard coded. working correctly
+        NetworkStream stream = client.GetStream();
+
+        FileStream inputFile = new($"{Constants.ServerOutputFolder}/IMG-20250318-WA0001.jpg", FileMode.Create);
+
+        await stream.CopyToAsync(inputFile);
+        client.Close();
+    }
+
+    internal static async Task ProcessClient2(TcpClient client)
+    {
+        // v2.1 hard coded. working correctly.
+        NetworkStream stream = client.GetStream();
+
+        FileStream inputFile = new($"{Constants.ServerOutputFolder}/IMG-20250318-WA0001.jpg", FileMode.Create);
+
+        int filesize = 90674;
+        byte[] bytes = new byte[filesize];
+
+        int readBytes = stream.Read(bytes, 0, filesize);
+        await inputFile.WriteAsync(bytes, 0, filesize);
+        client.Close();
     }
 
     internal static async Task ProcessClient(TcpClient client)
     {
+        // v1. not working
         NetworkStream stream = client.GetStream();
 
-        StreamReader streamReader = new(stream);
+        StreamReader streamReader = new(stream, Encoding.UTF8);
         string? metadataString = await streamReader.ReadLineAsync();
         ArgumentException.ThrowIfNullOrWhiteSpace(metadataString);
         var metadata = JsonSerializer.Deserialize<Dictionary<string, string>>(metadataString);
         ArgumentNullException.ThrowIfNull(metadata);
         int filesize = int.Parse(metadata["filesize"]);
         Console.WriteLine(metadata["filename"]);
+        Console.WriteLine(filesize);
 
         FileStream inputFile = new($"{Constants.ServerOutputFolder}/{metadata["filename"]}", FileMode.Create);
 
--- Sender/Client.cs
+++ Sender/Client.cs
@@ -9,6 +9,31 @@ internal static class Client
 {
     internal static async Task Run()
     {
+        // v3
+        using TcpClient client = new TcpClient("127.0.0.1", 13000);
+        var channel = client.GetStream();
+
+        using FileStream fileStream = new(Constants.FilePath, FileMode.Open);
+
+        string emo = "🙃";
+        StreamWriter streamWriter = new(channel,Encoding.UTF8);
+        streamWriter.WriteLine(emo);
+        streamWriter.Flush();
+        await fileStream.CopyToAsync(channel);
+    }
+    internal static async Task RunX()
+    {
+        // v2.1. working correctly
+        using TcpClient client = new TcpClient("127.0.0.1", 13000);
+        var channel = client.GetStream();
+
+        using FileStream fileStream = new(Constants.FilePath, FileMode.Open);
+
+        await fileStream.CopyToAsync(channel);
+    }
+    internal static async Task Run0()
+    {
+        // v1. not working
         // Preparing metadata
         using FileStream fileStream = new(Constants.FilePath, FileMode.Open);
         string filename = Path.GetFileName(fileStream.Name);
